eecfb8e1da56c1bc2f584a40009f0d7100eaf1d8
Semmozhi2008-04-13 <asemmozhi132008@gmail.com>
2026-06-08 22:32:17 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -27,11 +27,6 @@ from backend.models import (
 )
 import schemas as schemas
 
-from routes import auth
-app.include_router(auth.router, prefix="/api")
-
-
-
 class JobCreateRequest(BaseModel):
     jobTitle: str
     department: str
